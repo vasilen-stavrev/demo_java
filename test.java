824d03c9dffc9b13a55ab849eca0b2e458b6eede
vasilen-stavrev <82938627+vasilen-stavrev@users.noreply.github.com>
2021-04-26 18:18:02 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,28 @@
+package com.javatask;
+  
+public class People {  
+  private int id;  
+  private String fullName;
+  private String pin;
+  
+public int getId() { 
+    return id;  
+}  
+public void setId(int id) {  
+    this.id = id;  
+}  
+public String getFullName() {  
+    return firstName;  
+}  
+public void setFullName( String fullName ) {  
+    this.fullName = fullName;  
+}  
+public String getPIN() {  
+    return lastName;  
+}  
+public void setPIN(String pin) {  
+    this.pin = pin;  
+}  
+  
+  
+}  
